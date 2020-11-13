091fe3a12bb9508d486865b879039d3f789367d7
Tara Dia <taracdia@gmail.com>
2020-11-13 06:10:41 -0600

    downgraded react to v16 to prevent enzyme breaking

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import Account from "./Account.js"
 
-it("renders without crashing", () => {
-  shallow(<App />);
+it("renderss without crashing", () => {
+  mount(<App />);
 });
 
 it("renders Account header", () => {
   const wrapper = shallow(<App />);
   const welcome = <h1>Display Active Users Account Details</h1>;
   expect(wrapper.contains(welcome)).toEqual(true);
-})
+});
+
+const user = {
+  name: "Adeneye David",
+  email: "david@gmail.com",
+  username: "Dave"
+};
+
+// describe("<Account />", () => {
+//   it("contains account", () => {
+//     const wrapper = mount(<Account />);
+//     // const value = wrapper.find("p").text();
+//     // expect(value).toEqual("david@gmail.com");
+//   });
+//   // it("accepts user account props", () => {
+//   //   const wrapper = mount(<Account user={user} />);
+//   //   expect(wrapper.props().user).toEqual(user);
+//   // });
+// });
