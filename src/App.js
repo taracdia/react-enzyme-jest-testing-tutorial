d4ef06e3e78137948631d6ba5cd82d9d588e2585
Tara Dia <taracdia@gmail.com>
2020-11-12 18:18:49 -0600

    initial setup with mini app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Account from "./Account";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    isLoading: true,
+    users: [],
+    error: null,
+  };
+
+  fetchUsers() {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          users: data,
+          isLoading: false,
+        })
+      )
+      .catch((error) => this.setState({ error, isLoading: false }));
+  }
+
+  componentDidMount() {
+    this.fetchUsers();
+  }
+
+  render() {
+    const { isLoading, users, error } = this.state;
+    return (
+      <React.Fragment>
+        <h1>Display Active Users Account Details</h1>
+        {error ? <p>{error.message}</p> : null}
+        {!isLoading ? (
+          users.map((user) => {
+            return <Account key={user.username} user={user} />;
+          })
+        ) : (
+          <h3>Fetching Users...</h3>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
